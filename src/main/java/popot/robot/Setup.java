ca00f6c908a7ad68ce8b2dced702f9788fe9c7bd
YauheniPo <ip.popovich@mail.ru>
2019-04-15 14:10:37 -0700

    Created project for JAVA with Robot Framework

--- src/main/java/popot/robot/Setup.java
+++ src/main/java/popot/robot/Setup.java
@@ -1,5 +1,6 @@
 package popot.robot;
 
+import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
 
@@ -7,7 +8,8 @@ import org.robotframework.javalib.annotation.RobotKeywords;
 public class Setup {
 
     @RobotKeyword
-    public void setDriver() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver.exe");
+    @ArgumentNames({"driver", "path"})
+    public void setDriver(String driver, String path) {
+        System.setProperty(driver, System.getProperty("user.dir") + path);
     }
 }
--- src/test/robotframework/acceptance/Test_Case_1.robot
+++ src/test/robotframework/acceptance/Test_Case_1.robot
@@ -1,11 +1,10 @@
 *** Settings ***
-Documentation  A resource file containing the application specific keywords
-Library		Selenium2Library
-Library     org.robotframework.javalib.library.ClassPathLibrary      popot/robot/**.class
-Library     org.robotframework.javalib.library.AnnotationLibrary     popot/robot/**.class
+Documentation  Demo Test
+
+Resource    Base.robot
 
 *** Test Cases ***
-Check out joe colantonio dot com
-    Set Driver
-	Open Browser   https://www.google.com   chrome
+Demo test
+    Set Driver       driver=${driver}    path=${path}
+	Open Browser     url=${url}          browserName=${browser}
 	Close Browser
